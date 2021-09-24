42d536a76f4ddf9bd273305f7cb850805bfd14dd
Jr <Lijruiii@gmail.com>
2021-09-24 11:49:16 -0400

    Another test.

---
+++ src/BuddyInfo.java
@@ -0,0 +1,29 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BuddyInfo() {
+        this.name = "Unknown";
+        this.address = "No information";
+        this.phoneNumber = "xxx-xxx-xxxx";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public static void main(String[] args) {
+        BuddyInfo testHomer = new BuddyInfo("Homer", "Unknown Address", "111-111-1111");
+        System.out.println("Hello " + testHomer.getName());
+        System.out.println("test");
+    }
+}
