960dc04fbbea54296585e64759bd9f73e061bab7
Jr <Lijruiii@gmail.com>
2021-09-24 01:39:36 -0400

    Add a new comment test

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,23 +3,28 @@ import java.util.ArrayList;
 public class AddressBook {
 
     private ArrayList<BuddyInfo> collectionOfBuddyInfo;
-
     public AddressBook() {
-        this.collectionOfBuddyInfo = new ArrayList<BuddyInfo>();
+        this.collectionOfBuddyInfo = new ArrayList<>();
     }
 
     public void addBuddy(BuddyInfo needAdd) {
-        collectionOfBuddyInfo.add(needAdd);
+        if (needAdd != null) {
+            collectionOfBuddyInfo.add(needAdd);
+        }
     }
 
-    public void removeBuddy(BuddyInfo needRemove) {
-        collectionOfBuddyInfo.remove(needRemove);
+    public BuddyInfo removeBuddy(int index) {
+        if (index >=0 && index < collectionOfBuddyInfo.size()) {
+            return collectionOfBuddyInfo.remove(index);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
+        System.out.println("New comment");
     }
 }
